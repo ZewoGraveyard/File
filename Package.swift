abd2cc2a1687c7b686aa670a32b7c4413b6a03f0
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-05-07 16:13:59 +0900

    Update for 2016-04-03 Snapshot (#12)
    Update for 2016-05-03 Snapshot

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "File",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 5),
     ]
 )
