c6552958f91fbe7585de4317f77414e8e9ab000f
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-03 15:04:14 +0200

    Updated Venice and Swift (#16)
    * Updated Venice and Swift
    
    * Removed old travis flags

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "File",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 5),
     ]
